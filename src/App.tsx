c78a3eec19ac90b1fcdb70980d12d986c6d76053
Albus-489 <bavrzar@gmail.com>
2023-03-17 10:45:30 +0200

    navbar and new routes logic

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,93 @@ import PlayerPage from "./components/player/Player";
 function App() {
   return (
     <div className="App">
-      <header className="container-fluid p-4">
-        <div className="row">
+      <header className="">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              yoUr DIgiTal OaSis
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/tierlist">
+                    Tier-list
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="player">
+                    Player
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Additional links
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tierlist" element={<TierListPage />} />
+          <Route path="/player" element={<PlayerPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div className="row">
           <div className="col">
             <div className="home">
               <a className="mylinks" href="/">
@@ -40,16 +125,5 @@ function App() {
               </a>
             </div>
           </div>
-        </div>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tierlist" element={<TierListPage />} />
-        <Route path="/player" element={<PlayerPage />} />
-      </Routes>
-    </div>
-  );
+        </div> */
 }
-
-export default App;
