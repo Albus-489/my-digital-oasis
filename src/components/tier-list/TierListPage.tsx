337fe39015d6faf6060555b89a0bae7dfc7d41a6
Albus-489 <bavrzar@gmail.com>
2023-03-18 09:01:53 +0200

    model changes

--- src/components/tier-list/TierListPage.tsx
+++ src/components/tier-list/TierListPage.tsx
@@ -1,21 +1,28 @@
 import React, { DragEvent, FC, useEffect, useState } from "react";
 import ITierList from "./models/ITierList";
 import data from "./data/items.json";
+import "./styles/tierlist.component.css";
+const pepeImage = require("./images/pepestare.jpg");
 
 const TierListPage = () => {
   const [tiers, setTierList] = useState<ITierList[]>([
-    { name: "S", items: ["Half-life", "Hollow Knight"] },
+    { name: "S", items: [] },
     { name: "A", items: [] },
     { name: "B", items: [] },
     { name: "C", items: [] },
     { name: "D", items: [] },
+    { name: "Pool", items: [] },
   ]);
 
   const [items, setItems] = useState<string[]>([]);
 
   useEffect(() => {
-    setItems(data.games);
-    // console.log(data.games);
+    let _data = data.games;
+    let updatedPool = [...tiers];
+
+    updatedPool[5].items = _data.map((name) => ({ name, image: pepeImage }));
+    setTierList(updatedPool);
+    console.log(updatedPool);
   }, []);
 
   const onDragStart = (event: DragEvent, name: string) => {
@@ -30,66 +37,79 @@ const TierListPage = () => {
 
   const onDrop = (event: DragEvent<HTMLDivElement>, tierIndex: number) => {
     const itemName = event.dataTransfer.getData("text"); //which item was selected
-    const itemIndex = tiers.findIndex((tier) => tier.items.includes(itemName)); // index of the selected item
+    const originTierIndex = tiers.findIndex((tier) =>
+      tier.items.some((item) => item.name === itemName)
+    ); // index of origin tier
 
-    if (itemIndex !== -1) {
+    const itemsPoolIndex = items.indexOf(itemName); // index of item
+
+    if (originTierIndex !== -1) {
       const updatedTiers = [...tiers]; // copy the array
 
-      const item = updatedTiers[itemIndex].items.find(
-        (item) => item === itemName
+      const item = updatedTiers[originTierIndex].items.find(
+        (item) => item.name === itemName
       ); // find the item that will be moved
 
-      updatedTiers[itemIndex].items = updatedTiers[itemIndex].items.filter(
-        (item) => item !== itemName
-      ); // remove the item from the origin array
+      updatedTiers[originTierIndex].items = updatedTiers[
+        originTierIndex
+      ].items.filter((item) => item.name !== itemName); // remove the item from the origin array
 
       updatedTiers[tierIndex].items.push(item!); // add the item to the destination array
       setTierList(updatedTiers); // update the state
     }
+
+    if (itemsPoolIndex !== -1) {
+      const updatedTiers = [...tiers]; // copy the array
+    }
+
+    console.log(itemsPoolIndex);
   };
 
   return (
     <div>
       <h2>Tier List</h2>
       <div className="tiers">
-        {tiers.map((tier, tierIndex) => (
-          <div
-            key={tier.name}
-            className="tier"
-            onDragOver={(event) => onDragOver(event)}
-            onDrop={(event) => onDrop(event, tierIndex)}
-          >
-            <h3>{tier.name}</h3>
-            <ul>
-              {tier.items.map((item) => (
-                <li
-                  key={item}
-                  draggable
-                  onDragStart={(event) => onDragStart(event, item)}
-                >
-                  {item}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        {tiers.map((tier, tierIndex) =>
+          tierIndex !== 5 ? (
+            <div
+              key={tier.name}
+              className="tier"
+              onDragOver={(event) => onDragOver(event)}
+              onDrop={(event) => onDrop(event, tierIndex)}
+            >
+              <h3>{tier.name}</h3>
+              <ul>
+                {tier.items.map((item) => (
+                  <div
+                    key={item.name}
+                    draggable
+                    onDragStart={(event) => onDragStart(event, item.name)}
+                  >
+                    {item.name}
+                    <img src={item.image} alt="" />
+                  </div>
+                ))}
+              </ul>
+            </div>
+          ) : null
+        )}
       </div>
 
-      <div className="items">
-        <div className="itemsPool">
-          <div className="row flex-row">
-            {items.map((item, index) => (
-              <div className="col" key={index}>
-                <li
-                  key={index}
-                  draggable
-                  onDragStart={(event) => onDragStart(event, item)}
-                >
-                  {item}
-                </li>
+      <div className="itemsPool">
+        <h3>Pool</h3>
+        <div className="row flex-row row-cols-6">
+          {tiers[5].items.map((item, index) => (
+            <div className="col" key={index}>
+              <img src={item.image} alt="" />
+              <div
+                key={index}
+                draggable
+                onDragStart={(event) => onDragStart(event, item.name)}
+              >
+                {item.name}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/tier-list/models/ITierList.ts
+++ src/components/tier-list/models/ITierList.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 interface ITierList {
   name: string;
-  items: string[];
+  items: { name: string; image: string }[];
 }
 
 export default ITierList;
