9f0e2f9ec5e53b4f6edc8d9d83fe98bedb3e22b3
Albus-489 <bavrzar@gmail.com>
2023-03-20 14:12:00 +0200

    Testing new components logic

---
+++ src/components/test/models/ITier.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface ITier {
+  name: string;
+  items: { name: string; image: string }[];
+}
+
+export default ITier;
---
+++ src/components/test/tst-components/Pool.component.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+type TierProps = {
+  name: string;
+  items: { name: string; image: string }[];
+  onDragStart: (event: React.DragEvent<HTMLDivElement>, name: string) => void;
+  onDragOver: (event: React.DragEvent<HTMLDivElement>) => void;
+  onDrop: (event: React.DragEvent<HTMLDivElement>, tierIndex: number) => void;
+  tierIndex: number;
+};
+
+const Tier: React.FC<TierProps> = ({
+  name,
+  items,
+  onDragStart,
+  onDragOver,
+  onDrop,
+  tierIndex,
+}) => {
+  return (
+    <div
+      className="pool"
+      onDragOver={onDragOver}
+      onDrop={(event) => onDrop(event, tierIndex)}
+    >
+      <h2 className="poolName">{name}</h2>
+      {items.map((item) => (
+        <div
+          className="item"
+          key={item.name}
+          draggable
+          onDragStart={(event) => onDragStart(event, item.name)}
+        >
+          <img src={item.image} alt={item.name} />
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Tier;
