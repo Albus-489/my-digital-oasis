d5dd3cac3039d56feb5d422e0b9dd4f3e3a70b29
Albus-489 <bavrzar@gmail.com>
2023-03-21 20:13:52 +0200

    upload tierList func

--- src/components/tier-list/funcs/save-upload-tier.functions.ts
+++ src/components/tier-list/funcs/save-upload-tier.functions.ts
@@ -1,5 +1,6 @@
 import ITierList from "../models/ITierList";
 import { saveAs } from "file-saver";
+import ITier from "../../test/models/ITier";
 
 export const downloadFile = (list: ITierList[]): void => {
   const jsonData: string = JSON.stringify(list);
@@ -7,4 +8,25 @@ export const downloadFile = (list: ITierList[]): void => {
   saveAs(blob, "tierList.json");
 };
 
-export const uploadFile = (list: string): void => {};
+export const uploadFile = (
+  event: React.ChangeEvent<HTMLInputElement>,
+  setTierList: React.Dispatch<React.SetStateAction<ITier[]>>
+): void => {
+  const file = event.target.files?.[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      try {
+        const data = JSON.parse(reader.result as string) as ITierList[];
+        setTierList(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    reader.readAsText(file);
+  }
+};
