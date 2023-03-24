dfb72ab4d25e36c36c1992024133361df541165c
Albus-489 <bavrzar@gmail.com>
2023-03-24 14:31:33 +0200

    testing main page and create function

--- src/components/book-analysis/ba-main-page.component.tsx
+++ src/components/book-analysis/ba-main-page.component.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import { log } from "console";
+import React, { useEffect, useState } from "react";
+import { createBook } from "./funcs/createBook";
+import { Book } from "./models/Books";
+import { IBook } from "./models/interfaces/IBook";
+import "./styles/books-row.css";
+const boockCover = require("./images/bookCover.png");
 
 const BookAnalysisMainPage = () => {
+  const [books, setBooks] = useState<IBook[]>([]);
+
+  const logs = () => {
+    const b = new Book("Aboba");
+    const notes = b.getFullNotes();
+    console.log(notes);
+  };
   return (
     <div className="wrapper">
-      <p>Praise the sunnnnnnnnnnnnnnn!</p>
+      {/* BOOKS ROW */}
+      <div className="books-row">
+        {books.map((book, index) => (
+          <div className="card bg-dark" key={index}>
+            <img
+              id="book-image"
+              src={boockCover}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <a href="#" className="btn btn-primary">
+                Open
+              </a>
+            </div>
+          </div>
+        ))}
+        <div className="addNewBAbox">
+          <span
+            className="addNewBAtext"
+            onClick={() => createBook(new Book("Name"), books, setBooks)}
+          >
+            + Add
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/book-analysis/funcs/createBook.ts
@@ -0,0 +1,15 @@
+import { IBook } from "../models/interfaces/IBook";
+
+export const createBook = (
+  newBook: IBook,
+  books: IBook[],
+  setBooks: React.Dispatch<React.SetStateAction<IBook[]>>
+): void => {
+  let tempBooks = books;
+  tempBooks.push(newBook);
+  console.log("This function is called!");
+  console.log("Temp => ", tempBooks);
+
+  setBooks(tempBooks);
+  console.log("Original => ", books);
+};
