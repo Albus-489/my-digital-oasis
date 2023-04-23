fd1d24d1ad9e602e5c19476339f64538575e6e4a
Albus-489 <bavrzar@gmail.com>
2023-04-23 15:15:48 +0300

    axi q test

--- src/components/book-analysis/ba-main-page.component.tsx
+++ src/components/book-analysis/ba-main-page.component.tsx
@@ -1,11 +1,12 @@
-import { log } from "console";
 import React, { useEffect, useState } from "react";
 import { createBook } from "./funcs/createBook";
 import { Book } from "./models/Books";
 import { IBook } from "./models/interfaces/IBook";
 import "./styles/books-row.css";
+import Button from "react-bootstrap/Button";
 import BooksRow from "./components/books-row.component";
 import ModalComponent from "./components/modal.component";
+import getBooks from "./funcs/axios/getBooks";
 const boockCover = require("./images/bookCover.png");
 
 const BookAnalysisMainPage = () => {
@@ -17,13 +18,20 @@ const BookAnalysisMainPage = () => {
     console.log(notes);
   };
 
-  const testCreateBook = (newBook: IBook) => {
-    let tmpBook = [...books];
-    tmpBook.unshift(newBook);
-    setBooks(tmpBook);
+  const testGetBook = async () => {
+    const a = await getBooks();
+    console.log(a);
   };
   return (
     <div className="wrapper">
+      <Button
+        onClick={() => testGetBook()}
+        className="mt-5"
+        size="lg"
+        variant="primary"
+      >
+        GetBooks
+      </Button>
       {/* BOOKS ROW */}
       <BooksRow books={books} />
 
